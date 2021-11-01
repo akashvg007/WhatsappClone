b053a9139b5931f41e7e7335b55fe71ba4fece69
Akash VG <akashvg007@gmail.com>
2021-11-01 23:34:16 +0530

    chat is now available

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import LeftHandMenu from "./components/LeftContainer/LeftContainer";
 import MainContainer from './components/MainContainer/MainContainer';
 import LandingPage from './components/LandingPage/LandingPage';
 import Register from './components/Register/Register';
+import { getRecentChats } from "./Api/services";
+import { getContact } from "./Api/services";
 
 function App() {
   const [selected, setSelected] = useState(false);
   const [registered, setRegistered] = useState(false);
-  const [userData, setUserData] = useState({})
-  const handleClick = (data) => {
-    console.log("handleClick::clicked", data);
-    setSelected(true);
-    setUserData(data);
+  const [currentUser, setCurrentUser] = useState({})
+  const [chatMsgs, setChatMsgs] = useState([])
+  const [chatlist, setChatlist] = useState({})
+  const [contact, setContact] = useState({});
+  console.log("chats", chatMsgs);
+  const getChats = async () => {
+    const chats = await getRecentChats();
+    console.log("getRecentChats::chats", chats);
+    setChatMsgs(chats);
+    const phone = localStorage.getItem("phone") || ""
+    const users = {};
+    chats.map(chat => {
+      const { from, to, msg, time, status } = chat;
+      const user = from === phone ? to : from;
+      if (!users[user]) users[user] = [];
+      const obj = { msg, time, status, from, to };
+      users[user].push(obj);
+    })
+    console.log("getRecentChats::users", users);
+    setChatlist(users)
   }
+  const handleClick = (name) => {
+    console.log("handleClick::clicked", name);
+    setSelected(true);
+    setCurrentUser(name);
+  }
+  const getAllContacts = async () => {
+    const contacts = await getContact();
+    const contactList = {}
+    contacts.forEach(list => {
+      const { name, phone } = list;
+      if (!contactList[phone]) contactList[phone] = name;
+    })
+    setContact(contactList);
+  }
+
+  useEffect(() => {
+    if (registered) getChats();
+  }, [registered])
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setRegistered(true);
+      getAllContacts()
+    }
+  }, [])
   return (
     <div className="App">
       <div className="back-header"></div>
@@ -22,11 +64,11 @@ function App() {
         {registered ? (
           <div className="container">
             <div className="lefthandmenu">
-              <LeftHandMenu click={handleClick} />
+              <LeftHandMenu click={handleClick} contact={contact} list={chatlist} />
             </div>
             <div className="main-area">
               <div className="bglayer">
-                {selected ? <MainContainer data={userData} />
+                {selected ? <MainContainer contact={contact} phone={currentUser} data={chatlist[currentUser]} />
                   : <LandingPage />
                 }
               </div>
--- src/components/ChatPeople/ChatPeople.jsx
+++ src/components/ChatPeople/ChatPeople.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./ChatPeople.scss";
 import { Done, DoneAll } from '@mui/icons-material';
+import moment from 'moment';
+
 export default function ChatPeople(props) {
-    const { data, click } = props;
-    const { name, photo, lastMsg, status, time, id } = data;
+    const { data, click, name, contact } = props;
+    const [lastMsg, setLastMsg] = useState({});
+    // const [contactName, setContactName] = useState("");
+    const photo = props.photo || "avatar.jpg";
+
     console.log("props check", props);
     const handleClick = e => {
         console.log("chatpeople clicked");
-        click(data);
+        click(name);
     }
-
+    const getLastMessage = () => {
+        if (!data || !Array.isArray(data)) return;
+        const last = data[0];
+        data.forEach(chat => {
+            if (chat.time > last.time) last = chat;
+        })
+        setLastMsg(last);
+    }
+    // useEffect(() => {
+    //     console.log("contact name", contactName)
+    //     contact ? setContactName(contact) : setContactName(name);
+    // }, [contact])
+    useEffect(() => {
+        getLastMessage();
+    }, [])
     return (
         <div id="chatpeople" onClick={handleClick}>
             <div className="pic">
@@ -18,23 +37,23 @@ export default function ChatPeople(props) {
             <div className="details">
                 <div className="title">
                     <div className="name">
-                        {name}
+                        {contact || name}
                     </div>
                     <div className="lastMsg">
                         <div className="status">
                             {
-                                status == 1 ?
+                                lastMsg?.status == 1 ?
                                     <Done />
                                     : <DoneAll />
                             }
                         </div>
                         <div className="message">
-                            {lastMsg}
+                            {lastMsg?.msg?.substring(0, 20) + "..."}
                         </div>
                     </div>
                 </div>
                 <div className="time">
-                    {time}
+                    {moment(lastMsg.time).format('LT')}
                 </div>
             </div>
         </div>
